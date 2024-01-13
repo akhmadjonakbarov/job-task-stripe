07b7604cdfe461a5c9021e813d61e81e678639fd
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-01-14 00:13:27 +0500

    init

--- stripe_app/urls.py
+++ stripe_app/urls.py
@@ -5,7 +5,7 @@ from .views import get_stripe_session, item_detail, get_all_items, success_view,
 urlpatterns = [
     path('buy/<int:item_id>/', get_stripe_session, name='get_stripe_session'),
     path('item/<int:item_id>/', item_detail, name='item_detail'),
-     path('', get_all_items, name='get_all_items'),  
-      path('success/', success_view, name='success'),  # Add your success view
+    path('', get_all_items, name='get_all_items'),  
+    path('success/', success_view, name='success'),  # Add your success view
     path('cancel/', cancel_view, name='cancel'),
 ]
